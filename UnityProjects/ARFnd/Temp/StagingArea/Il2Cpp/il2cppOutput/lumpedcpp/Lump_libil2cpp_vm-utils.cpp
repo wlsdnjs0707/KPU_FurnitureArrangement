ac76eb4d6b0ac12324e7e77a8acdbabbf1cf65f9
wlsdnjs0707 <86781939+wlsdnjs0707@users.noreply.github.com>
2022-03-13 07:40:46 +0900

    Delete UnityProjects/ARFnd directory

--- UnityProjects/ARFnd/Temp/StagingArea/Il2Cpp/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\BlobReader.cpp"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\Debugger.cpp"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "D:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Data\il2cpp\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
